db9c67b3f85ca4a5b4ae30b197913832cfcdcecd
Scott Dietrich <scdietrich@gmail.com>
2015-07-01 12:23:09 -0700

    Refactor module, add additional test cases.

--- index.js
+++ index.js
@@ -2,101 +2,110 @@
 
 var replicationStream = require('pouchdb-replication-stream');
 var quickconnect = require('rtc-quickconnect')
-var createDataStream = require('rtc-dcstream');
+var RtcDataStream = require('rtcstream');
 var Promise = require('promise');
 var PouchDB = require('pouchdb');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
-module.exports = function(signalUrl, rtcOptions, pouchDb, replicationOptions) {
-  var _quickconnect, _signalUrl, _rtcOptions, _pouchDb, _replicationOptions;
-  _signalUrl = signalUrl;
-  _rtcOptions = rtcOptions;
-  _pouchDb = pouchDb;
-  _replicationOptions = replicationOptions;
+var PouchReplicator = function(name, signalUrl, rtcOptions, pouchDb, replicationOptions) {
+  this.name = name;
+  this.signalUrl = signalUrl;
+  this.rtcOptions = rtcOptions;
+  this.pouchDb = pouchDb;
+  this.replicationOptions = replicationOptions;
 
-  var _streams = [];
-  var _peers = [];
+  this.streams = [];
+  this.peers = [];
 
+  EventEmitter.call(this);
   PouchDB.plugin(replicationStream.plugin);
   PouchDB.adapter('writableStream', replicationStream.adapters.writableStream);
+};
 
-  function addPeer(id, dc) {
-    var stream = createDataStream(dc);
-    _peers.push(id);
-    _streams.push(stream);
+util.inherits(PouchReplicator, EventEmitter);
 
-    stream.on('end', function() {
-      _pouchDb.load(stream, _replicationOptions)
-      .catch(function(error) {
-        console.log(error.stack);
-      });
-    });
-  }
+PouchReplicator.prototype.addPeer = function(id, dc) {
+  var self = this;
 
-  function removePeer(id) {
-    var idx = _peers.indexOf(id);
-    if (idx >= 0) {
-      _peers.splice(idx, 1);
-      _streams.splice(idx, 1);
-    }
-  }
+  var stream = new RtcDataStream(this.name + ':' + id, dc);
+  this.peers.push(id);
+  this.streams.push(stream);
 
-  return {
-    /**
-     * Join webrtc datachannel
-     @ @return  {Promise}
-     */
-    join: function(minPeers) {
-      minPeers = typeof minPeers !== 'undefined' ? minPeers : 0;
+  stream.on('readable', this.receiveData.bind(this, stream));
+}
 
-      var room = 'pouch';
-      var p = new Promise(function(resolve, reject) {
-        _quickconnect = quickconnect(_signalUrl, rtcOptions);
-        _quickconnect.createDataChannel(room)
-          .on('channel:opened:' + room, function(id, dc) {
-            addPeer(id, dc);
+PouchReplicator.prototype.receiveData = function(s) {
+  var self = this;
 
-            if (_peers.length >= minPeers) {
-              resolve();
-            }
-          })
-          .on('channel:closed:' + room, function(id, dc) {
-            removePeer(id);
-          });
-      });
+  self.pouchDb.load(s, this.replicationOptions)
+  .then(function(res) {
+    self.emit('load');
+  });
+};
 
-      return p;
-    },
-
-    /**
-     * Start PouchDB replication
-     @ @return  {Promise}
-     */
-    replicate: function() {
-      var replicationPromises = [];
-      _streams.forEach(function(s) {
-        replicationPromises.push(_pouchDb.dump(s));
-      });
-
-      var p = new Promise(function(resolve, reject) {
-        Promise.all(replicationPromises)
-        .then(function () {
-          resolve();
-        });
-      })
-
-      return p;
-    },
-
-    close: function() {
-      _quickconnect.close();
-    },
-
-    getPeers: function() {
-      return _peers;
-    },
-
-    on: function(callback) {
-      _quickconnect.on(callback);
-    }
+PouchReplicator.prototype.removePeer = function(id) {
+  var idx = this.peers.indexOf(id);
+  if (idx >= 0) {
+    this.peers.splice(idx, 1);
+    this.streams.splice(idx, 1);
   }
 };
+
+/**
+ * Join webrtc datachannel
+ @ @return  {Promise}
+ */
+PouchReplicator.prototype.join = function(minPeers) {
+  minPeers = typeof minPeers !== 'undefined' ? minPeers : 0;
+  var self = this;
+
+  var p = new Promise(function(resolve, reject) {
+    self.signaller = quickconnect(self.signalUrl, self.rtcOptions);
+    self.signaller.createDataChannel(self.rtcOptions.room)
+      .on('channel:opened:' + self.rtcOptions.room, function(id, dc) {
+        self.addPeer(id, dc);
+        if (self.peers.length >= minPeers) {
+          resolve();
+        }
+      })
+      .on('channel:closed:' + self.rtcOptions.room, function(id, dc) {
+        self.removePeer(id);
+      });
+  });
+
+  return p;
+};
+
+/**
+ * Start PouchDB replication
+ @ @return  {Promise}
+ */
+PouchReplicator.prototype.replicate = function() {
+  var self = this;
+  var replicationPromises = [];
+
+  self.streams.forEach(function(s) {
+    var promise = self.pouchDb.dump(s);
+    replicationPromises.push(promise);
+  });
+
+  var p = new Promise(function(resolve, reject) {
+    Promise.all(replicationPromises)
+    .then(function () {
+      resolve();
+    });
+  })
+
+  return p;
+};
+
+PouchReplicator.prototype.close = function() {
+  this.signaller.close();
+};
+
+PouchReplicator.prototype.getPeers = function() {
+  return this.peers;
+};
+
+module.exports = PouchReplicator;
--- test/test.js
+++ test/test.js
@@ -8,17 +8,10 @@ var Promise = require('promise');
 var PouchDB = require('pouchdb');
 var PouchReplicator = require('../');
 
+var SegfaultHandler = require('segfault-handler');
+SegfaultHandler.registerHandler();
+
 describe('pouch-replicate-webrtc node module', function () {
-
-  var rtcOpts = {
-    room: 'pouch-replicate-test',
-    // debug: true,
-    plugins: [ require('rtc-plugin-node') ]
-  };
-
-  var replicationOptions = {
-    batch_size: 1
-  };
 
   var pouch1, pouch2, pouch3, replicator1, replicator2, replicator3;
   var doc1 = {_id: 'cats', name: 'cats'};
@@ -28,14 +21,37 @@ describe('pouch-replicate-webrtc node module', function () {
   var doc5 = {_id: 'tables', name: 'tables'};
   var doc6 = {_id: 'chairs', name: 'chairs'};
 
+  // TODO: this pause seems fragile, investigate better options
+  // pause to allow replication to complete
+  var pauseFunction = function() {
+    var p = new Promise(function (resolve, reject){
+      setTimeout(function () {
+        resolve();
+      }, 250)
+    });
+
+    return p;
+  };
+
   beforeEach(function(done) {
+    var rtcOpts = {
+      room: cuid(),
+      nick: 'foo',
+      // debug: true,
+      plugins: [ require('rtc-plugin-node') ]
+    };
+
+    var replicationOptions = {
+      batch_size: 1
+    };
+
     pouch1 = new PouchDB(cuid(), {db: require('memdown')});
     pouch2 = new PouchDB(cuid(), {db: require('memdown')});
     pouch3 = new PouchDB(cuid(), {db: require('memdown')});
 
-    replicator1 = new PouchReplicator('http://localhost:3000/', rtcOpts, pouch1, replicationOptions);
-    replicator2 = new PouchReplicator('http://localhost:3000/', rtcOpts, pouch2, replicationOptions);
-    replicator3 = new PouchReplicator('http://localhost:3000/', rtcOpts, pouch3, replicationOptions);
+    replicator1 = new PouchReplicator('replicator1', 'http://localhost:3000/', rtcOpts, pouch1, replicationOptions);
+    replicator2 = new PouchReplicator('replicator2', 'http://localhost:3000/', rtcOpts, pouch2, replicationOptions);
+    replicator3 = new PouchReplicator('replicator3', 'http://localhost:3000/', rtcOpts, pouch3, replicationOptions);
 
     Promise.all([replicator1.join(2), replicator2.join(2), replicator3.join(2)])
       .then(function() {
@@ -59,6 +75,11 @@ describe('pouch-replicate-webrtc node module', function () {
     replicator3.close();
   });
 
+  it('should find correct number of peers', function() {
+    var peers = replicator1.getPeers();
+    assert.equal(peers.length, 2);
+  });
+
   it('should find setup documents', function(done) {
     pouch1.allDocs({
       include_docs: true,
@@ -70,20 +91,20 @@ describe('pouch-replicate-webrtc node module', function () {
     });
   });
 
+  // TODO: this test fails because the readable stream given to
+  // pouchdb-replication-stream.load() appears to never end
+  it('should emit when done loading', function(done) {
+    replicator2.on('load', function() {
+      done();
+    });
+
+    Promise.all([replicator2.replicate(), replicator1.replicate(), replicator3.replicate(), pauseFunction()])
+    .catch(function(error) {
+      console.log(error.stack);
+    });
+  });
+
   it('should replicate', function (done) {
-
-    // TODO: this pause seems fragile, investigate better options
-    // pause to allow replication to complete
-    var pauseFunction = function() {
-      var p = new Promise(function (resolve, reject){
-        setTimeout(function () {
-          resolve();
-        }, 250)
-      });
-
-      return p;
-    };
-
     Promise.all([replicator2.replicate(), replicator1.replicate(), replicator3.replicate(), pauseFunction()])
     .then(function() {
       return pouch1.allDocs({
@@ -117,6 +138,28 @@ describe('pouch-replicate-webrtc node module', function () {
       console.log(error.stack);
     });
 
+  });
+
+  it('should replicate multiple times on the same stream', function (done) {
+    Promise.all([replicator2.replicate(), replicator1.replicate(), replicator3.replicate(), pauseFunction()])
+    .then(function() {
+      pouch1.put({_id: 'a', name: 'a'});
+    })
+    // TODO: trying to replicate a second time causes a segfault
+    // .then(Promise.all(replicator1.replicate(), pauseFunction()))
+    .then(function() {
+      return pouch2.allDocs({
+        include_docs: true,
+        attachments: true
+      })
+    })
+    .then(function(results) {
+      assert.equal(results.total_rows, 7);
+      done();
+    })
+    .catch(function(error) {
+      console.log(error.stack);
+    });
   });
 
 });
